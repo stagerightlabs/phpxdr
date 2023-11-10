06fd86e517e7219783a7a1a2a3b954be84e3b05d
Ryan C. Durham <ryan@stagerightlabs.com>
2023-11-10 11:51:37 -0800

    Remove redundant return types from interfaces

--- src/Interfaces/XdrEnum.php
+++ src/Interfaces/XdrEnum.php
@@ -13,24 +13,16 @@ interface XdrEnum
 {
     /**
      * Return the value to be encoded as XDR bytes.
-     *
-     * @return integer
      */
     public function getXdrSelection(): int;
 
     /**
      * Allow the XDR reader to create a new instance of this class.
-     *
-     * @param int $value
-     * @return static
      */
     public static function newFromXdr(int $value): static;
 
     /**
      * Determine if a value is a member of the ENUM options.
-     *
-     * @param integer $value
-     * @return boolean
      */
     public function isValidXdrSelection(int $value): bool;
 }
--- src/Interfaces/XdrStruct.php
+++ src/Interfaces/XdrStruct.php
@@ -15,17 +15,12 @@ interface XdrStruct
      * Return the object as an XDR string. The implementing class is responsible
      * for composing the XDR byte structure for this struct; the XDR write
      * method will apply those bytes directly to its buffer.
-     *
-     * @return void
      */
     public function toXdr(XDR &$xdr): void;
 
     /**
      * Allow the XDR reader to create a new instance of this class.
      * The implementing class must hydrate itself by reading bytes from $xdr.
-     *
-     * @param XDR $xdr
-     * @return static
      */
     public static function newFromXdr(XDR &$xdr): static;
 }
